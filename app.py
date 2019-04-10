24d3749ce8d4b0d0a7e756efd18973b08b58f280
sleter <sleter97@gmail.com>
2019-04-10 12:01:09 +0200

    req update

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import cv2 as cv
 from flask import Flask, render_template, send_from_directory
 from flask_socketio import SocketIO, emit
-import eventlet, base64
+import eventlet, base64, json
 eventlet.monkey_patch()
+
+SLEEP_TIME = 0.1
 
 app = Flask(__name__, static_url_path='')
 app.config['SECRET_KEY'] = 'secret!'
@@ -16,18 +18,28 @@ def serve_model(path):
         return send_from_directory('models', path)
 
 def send_coordinates():
-        pass
+        while True:
+                socketio.emit('coordinates', json.dumps({
+                        'id': 1,
+                        'x': 1,
+                        'y': 1,
+                        'z': 1,
+                        'rx': 0,
+                        'ry': 0,
+                        'rz': 0
+                        }))
+                eventlet.sleep(SLEEP_TIME)
 
 def stream_video():
-    while True:
-        _, frame = capture.read()
-        _, buffer = cv.imencode('.jpg', frame)
-        image = base64.b64encode(buffer).decode("utf-8") 
-        socketio.emit('image', image)
-        eventlet.sleep(0.1)
+        while True:
+                _, frame = capture.read()
+                _, buffer = cv.imencode('.jpg', frame)
+                image = base64.b64encode(buffer).decode("utf-8") 
+                socketio.emit('image', image)
+                eventlet.sleep(SLEEP_TIME)
 
 eventlet.spawn(stream_video)
 eventlet.spawn(send_coordinates)
 
 if __name__ == '__main__':
-    socketio.run(app, use_reloader=False)
+        socketio.run(app, use_reloader=False)
