fd4b240377b95ee9387543ddac23597c68b6d388
sleter <sleter97@gmail.com>
2019-04-10 12:25:14 +0200

    video streaming

--- card_recognition/cam_stream.py
+++ card_recognition/cam_stream.py
@@ -1,16 +1,12 @@
 import cv2
 
-
 class CardRecognition:
     def __init__(self):
-        self.camera = cv2.VideoCapture(1)
-
+        self.camera = cv2.VideoCapture(0)
 
     def create_cam_stream(self):
         self.success, self.frame = self.camera.read()
         return self.frame
-
-
 
 if __name__ == '__main__':
     cr = CardRecognition()
