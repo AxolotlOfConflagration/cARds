13f0166527eaaf4c934c51f867221481cdbcf992
Agulaa <agnieszkaxrusin@gmail.com>
2019-04-16 20:53:06 +0200

    update match method and add reading qrcode

--- card_recognition/cam_stream.py
+++ card_recognition/cam_stream.py
@@ -23,32 +23,61 @@ class CardRecognition:
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
+    def read_qr_code(self):
+        while True:
+            frame = self.create_cam_stream()
+            detector = cv2.QRCodeDetector()
+            data, points, straight_qrcode = detector.detectAndDecode(frame)
+            print(data)
+
     def show_matcher(self, image=None):
         frame = self.create_cam_stream()
-        min_matches = 20
-        sift = cv2.xfeatures2d.SIFT_create()  # Initiate keypoint SIFT detector
-        bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=False)
-
+        MIN_MATCHES = 20
+        #####################################################################
+        # Initiate SIFT detector #
+        sift = cv2.xfeatures2d.SIFT_create()
+        ####################################################################
         if image:
             img = image
         else:
             img=self.pikachu
-        keypoints_img, des_img = sift.detectAndCompute(img, None)
+        #####################################################################
+        # finds the keypoints and descriptors
+        # sift.detect() -> find image keypoints
+        # sift.compute() -> computes teh descriptors from the keypoints
+        # sift.detectAndCompute() -> directly find keypoints and descriptors
+        keypoints_img1, descriptors_img = sift.detectAndCompute(img, None)
+        ####################################################################
 
-
+        ####################################################
+        # Brute-force descriptor matcher
+        bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=False)
+        ####################################################q
         while True:
-
             cv2.imshow("Stream", frame)
             frame = self.create_cam_stream()
-            keypoints_frame, des_frame = sift.detectAndCompute(frame, None)
+            keypoints_frame, descriptors_frame = sift.detectAndCompute(frame, None)
 
-            matches = bf.match(des_img, des_frame)
+            ########################################################
+            # Make matches with description from image and current frame
+            matches = bf.match(descriptors_img, descriptors_frame)
+            ########################################################
+            # DMatch -> item.distance, item.trainIdx, item.queryIdx, item.imgIdx
+            # sorted by distance
             matches = sorted(matches, key=lambda x: x.distance)
-            if len(matches) > min_matches:
-                src_pts = np.float32([keypoints_img[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
-                dst_pts = np.float32([keypoints_frame[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+            if len(matches) > MIN_MATCHES:
+                # pt - keypoints coordinate x,y -> Point2f -- coordinates of the keypoint
+                # queryIdx -> from descriptors_img to keypoints_img (img1)
+                # trainIdx -> from descriptors_frame to keypoints_ (img2)
 
-                homography, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+                keypoints_image_xy = np.float32(
+                    [keypoints_img1[match.queryIdx].pt for match in matches[:MIN_MATCHES]]).reshape(-1, 1,
+                                                                                                    2)  # wiersze, kolumny, ilość elementów w kolumnie
+
+                keypoints_frame_xy = np.float32(
+                    [keypoints_frame[match.trainIdx].pt for match in matches[:MIN_MATCHES]]).reshape(-1, 1, 2)
+
+                homography, mask = cv2.findHomography(keypoints_image_xy, keypoints_frame_xy, cv2.RANSAC, 5.0)
                 #print('hom', homography)
                 heigh, weigh = img.shape
                 pts = np.float32([[0, 0], [0, heigh - 1], [weigh - 1, heigh - 1], [weigh - 1, 0]]).reshape(-1, 1, 2)
@@ -56,7 +85,7 @@ class CardRecognition:
 
                 frame = cv2.polylines(frame, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
                 cv2.imshow('frame', frame)
-                frame1 = cv2.drawMatches(img, keypoints_img, frame, keypoints_frame, matches[:10], 0, flags=2)
+                frame1 = cv2.drawMatches(img, keypoints_img1, frame, keypoints_frame, matches[:10], 0, flags=2)
                 cv2.imshow('frame1', frame1)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
