eea46287510debc69cfcb1e3300f3145a1d82a52
Agulaa <agnieszkaxrusin@gmail.com>
2019-05-08 13:28:37 +0200

    update cam stream and saveing camera parameters to json

--- card_recognition/cam_stream.py
+++ card_recognition/cam_stream.py
@@ -2,15 +2,18 @@ import cv2
 import numpy as np
 import glob
 import cv2.aruco as aruco
+import os
+import json
 
 class CardRecognition:
     def __init__(self, number):
         self.camera = cv2.VideoCapture(number)
-
         self.bulbasaur = 3
         self.charmander = 0
         self.pikachu = 2
         self.squirtle = 1
+        self.path = os.path.realpath('CameraCalibration/img/*.bmp').replace("\\", "/")
+
 
     def create_cam_stream(self):
         self.success, self.frame = self.camera.read()
@@ -22,7 +25,7 @@ class CardRecognition:
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
-    def calibrantion_cam_1(self, path):
+    def calibrantion_cam_1(self):
         # termination criteriaq
         criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
         # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
@@ -36,13 +39,13 @@ class CardRecognition:
         objpoints = []  # 3d point in real world space
         imgpoints = []  # 2d points in image plane.
 
-        for fname in glob.glob(path):
+        for fname in glob.glob(self.path):
+
             img = cv2.imread(fname)
-            # cv2.imshow(fname, img)
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
             # Find the chess board corners
-            ret, corners = cv2.findChessboardCorners(gray, (cbcol, cbrow), None)
+            ret, corners = cv2.findChessboardCorners(gray, (cbcol, cbrow), criteria)
 
             # If found, add object points, image points (after refining them)
             if ret == True:
@@ -57,6 +60,31 @@ class CardRecognition:
         ret, camera_matrix, dist_coeffs, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],
                                                                             None, None)
         return camera_matrix, dist_coeffs
+
+    def write_cam_parameters_to_file(self):
+        objpoints, imgpoints, gray = self.calibrantion_cam_1()
+
+        camera_matrix, dist_coeffs = self.get_cam_matrix(objpoints, imgpoints, gray)
+
+        camera = {
+            "camera_matrix": camera_matrix.tolist(),
+            "dist_coeff": dist_coeffs.tolist()
+        }
+
+        with open("camera.json", "w") as f:
+            json.dump(camera, f)
+
+    def get_camera_parameters_from_file(self):
+        try:
+            with open("camera.json", 'r') as f:
+                camera = json.load(f)
+                camera_matrix = np.array(camera['camera_matrix'])
+                dist_coeffs = np.array(camera['dist_coeff'])
+                return camera_matrix, dist_coeffs
+        except:
+            return np.array([]),np.array([])
+
+
 
     def detect_aruco(self, image):
         image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -74,34 +102,65 @@ class CardRecognition:
 
         return rvec, tvec
 
+    def get_camera_parameters(self):
+        camera_matrix, dist_coeffs = self.get_camera_parameters_from_file()
+        if  len(camera_matrix)==0:
+            self.write_cam_parameters_to_file()
+            camera_matrix, dist_coeffs = self.get_camera_parameters_from_file()
+        return  camera_matrix, dist_coeffs
     def get_corners_and_rortation_and_translation(self, frame):
-        objpoints, imgpoints, gray = self.calibrantion_cam_1(path)
-        camera_matrix, dist_coeffs = self.get_cam_matrix(objpoints, imgpoints, gray)
+        # objpoints, imgpoints, gray = self.calibrantion_cam_1()
+        # camera_matrix, dist_coeffs = self.get_cam_matrix(objpoints, imgpoints, gray)
+        camera_matrix, dist_coeffs = self.get_camera_parameters()
+
         corners, ids = self.detect_aruco(frame)
-        rvec, tvec, = self.get_corners_rotation_and_translation(corners, camera_matrix, dist_coeffs)
+        rvec, tvec = self.get_rotation_and_translation(corners, camera_matrix, dist_coeffs)
 
         return corners, rvec, tvec, ids
-    def show_asix(self, path):
 
-        objpoints, imgpoints, gray = self.calibrantion_cam_1(path)
-        camera_matrix, dist_coeffs = self.get_cam_matrix(objpoints, imgpoints, gray)
+    def test_cor_ror_tran(self):
+        while True:
+            frame = self.create_cam_stream()
+            corners, rvec, tvec, ids = self.get_corners_and_rortation_and_translation(frame)
+            print("corners")
+            print(corners)
+            print("rvec")
+            print(rvec)
+            print("tvec")
+            print(tvec)
+            print("ids")
+            print(ids)
+
+
+    def show_asix(self):
+
+        # objpoints, imgpoints, gray = self.calibrantion_cam_1()
+        # camera_matrix, dist_coeffs = self.get_cam_matrix(objpoints, imgpoints, gray)
+        camera_matrix, dist_coeffs = self.get_camera_parameters()
         while True:
             frame = self.create_cam_stream()
             corners, ids = self.detect_aruco(frame)
 
             rvec, tvec = self.get_rotation_and_translation(corners,camera_matrix, dist_coeffs)
             try:
-                for i in range(0, len(rvec)):
-                    cv2.aruco.drawAxis(frame, camera_matrix, dist_coeffs, rvec[i], tvec[i], 10)
-                cv2.imshow('test2', frame)
+                if len(rvec>0):
+                    for i in range(0, len(rvec)):
+                        cv2.aruco.drawAxis(frame, camera_matrix, dist_coeffs, rvec[i], tvec[i], 10)
+                    cv2.imshow('test2', frame)
             except:
                 pass
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
 
+
+
+
 if __name__ == '__main__':
     cr = CardRecognition(1)
-    path = "path_to_img"
+    cr.show_asix()
+    #cr.test_cor_ror_tran()
 
-    cr.show_asix(path)
+
+    #cr.write_cam_parameters_to_file()
+    #print(cr.get_camera_parameters_from_file())
